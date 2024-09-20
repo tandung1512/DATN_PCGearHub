3311b1e5a262d85c50e52a1299aabb020170458a
tandung1512 <tandung15122003@gmail.com>
2024-09-20 13:30:53 +0700

    add

---
+++ src/main/java/com/poly/asm/DatnPcGearHubApplication.java
@@ -0,0 +1,13 @@
+package com.poly.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnPcGearHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnPcGearHubApplication.class, args);
+	}
+
+}
